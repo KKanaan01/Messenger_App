a3cb67353aa7dbcf50a9aedaae255fd9c472f85c
Karim Kanaan <karimkanaan00@gmail.com>
2026-01-31 14:37:30 -0400

    nevermind now its added

--- server/src/app.js
+++ server/src/app.js
@@ -9,6 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/api/auth" , require('./routes/auth'));
+app.use("/api/chat" , require('./routes/conversation'));
 
 
 app.get("/" , (req , res) => {
